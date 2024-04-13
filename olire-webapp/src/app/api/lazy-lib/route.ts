ba5ebe3b88ca7c63b08eb1e9053e297d718376ce
John Tad <johntad110@gmail.com>
2024-04-13 13:14:36 +0300

    integrate Gemini API endpoint

--- olire-webapp/src/app/api/lazy-lib/route.ts
+++ olire-webapp/src/app/api/lazy-lib/route.ts
@@ -1,5 +1,8 @@
 import { getLibs } from "@/lib/actions/read_libs";
 import { NextRequest, NextResponse } from "next/server";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { getAllTags } from "@/lib/actions/read_tags";
+import { getAllPls } from "@/lib/actions/read_pls";
 
 export async function POST(req: NextRequest, res: NextResponse) {
     const body = await req.json()
@@ -7,11 +10,32 @@ export async function POST(req: NextRequest, res: NextResponse) {
 
     // Craft a prompt that makes gemini chose a tag and a proramming language 
     // from a list and assign it's coice to projData.
+    const genAI = new GoogleGenerativeAI(process.env.GGI_API_KEY ?? '');
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" })
 
-    const {tag, progLang} = {
-        tag: "Computer Vision",
-        progLang: "Python",
-    }
+    const tags = await getAllTags()
+    const pls = await getAllPls()
+
+    const tagNames = tags.map((tag: any) => tag.name)
+    const plsNames = pls.map((pl) => pl.name)
+
+    const prompt = `
+        From the following tags "${tagNames.join(', ')}" and the follwoing programming languages "${plsNames.join(', ')}"
+        which one is perfect for the project """${project}""". Return your answer in the
+        following json format:
+        {
+            "tag": "<TAG_HERE>",
+            "progLang": "<PROGRAMMIG_LANGUAGE_HERE>"
+        }
+        Rememebr the project is the one in triple quotes.
+    `;
+
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text()
+    const tagAndPl = extractJSON(text) ?? '{}'
+
+    const { tag, progLang } = JSON.parse(tagAndPl)
 
     if (tag !== undefined && progLang !== undefined) {
         const lib_result = await getLibs([tag], [progLang])
@@ -29,4 +53,10 @@ export async function POST(req: NextRequest, res: NextResponse) {
 
         return new Response(JSON.stringify(libraries))
     }
+}
+
+function extractJSON(text: string): string | null {
+    const regex = /{[^{}]*"tag"[^{}]*"progLang"[^{}]*}/
+    const match = text.match(regex)
+    return match ? match[0] : null;
 }
